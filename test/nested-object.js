88f1c123f709a367ea16defc0a76fb6d9b2c5fdc
Gautham Ramachandran <gautham2r@gmail.com>
2016-01-18 18:48:02 +0530

    More silly test cases

--- test/nested-object.js
+++ test/nested-object.js
@@ -10,11 +10,16 @@ describe('test nested object cases', () => {
     commentId: { type: 'alphaNumeric', required: true },
     postedAt: { type: 'date', required: true }
   });
+  let LikesSchema = new valydet.Schema({
+    id: { type: 'alphaNumeric', required: true },
+    likedAt: { type: 'date', required: true }
+  });
   let UserSchema = new valydet.Schema({
     id: { type: 'number', required: true },
-    comments: { type: CommentSchema, required: true }
+    comments: { type: CommentSchema, required: true },
+    likes: { type: 'array', of: LikesSchema }
   });
-  
+
   it('should validate commentId of comment schema', (done) => {
     let user = UserSchema.validate({
       id: 1,
@@ -22,7 +27,7 @@ describe('test nested object cases', () => {
         commentId: '!@!%'
       }
     });
-    
+
     let failures = user.failures();
     expect(failures).to.have.length.of.at.least(2);
     expect(failures[0]).to.have.ownProperty('key');
@@ -31,7 +36,7 @@ describe('test nested object cases', () => {
     expect(failures[1].key).to.equal('postedAt');
     done();
   });
-  
+
   it('should pass validation - comment schema', (done) => {
     let user = UserSchema.validate({
       id: 1,
@@ -41,6 +46,26 @@ describe('test nested object cases', () => {
       }
     });
     let failures = user.failures();
+    expect(failures).to.have.length(0);
+    done();
+  });
+
+  it('should validate type - array of - schemaType (LikesSchema)', (done) => {
+    let user = UserSchema.validate({
+      id: 1,
+      comments: {
+        commentId: 'A177s',
+        postedAt: new Date()
+      },
+      likes: [{
+        id: 'AS1125',
+        likedAt: new Date()
+      }]
+    });
+    let failures = user.failures();
+    expect(failures).not.to.be.null;
+    expect(failures).not.to.be.undefined;
+    expect(failures).to.be.instanceof(Array);
     expect(failures).to.have.length(0);
     done();
   });
